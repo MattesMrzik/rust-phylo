9fe1ab4f5b3a431781cd279cec13ba4bfcb1c538
Mattes Mrzik <mattes@mrzik.de>
2026-04-02 21:36:01 +0200

    implemented requested changes

--- phylo/src/pip_model/mod.rs
+++ phylo/src/pip_model/mod.rs
@@ -120,13 +120,11 @@ impl<Q: QMatrix> EvoModel for PIPModel<Q> {
     // This assumes correct dimensions to minimise runtime checks
     fn set_freqs(&mut self, pi: FreqVector) {
         debug_assert!(self.freqs.nrows() - 1 == pi.nrows() || self.freqs.nrows() == pi.nrows());
-        let old_subst_freqs = self.subst_q.freqs().clone();
-        self.subst_q.set_freqs(pi.clone());
-        if self.subst_q.freqs() != &old_subst_freqs {
-            self.freqs = pi.clone().insert_row(pi.nrows(), 0.0);
-            pip_q(&mut self.q, self.subst_q.q(), self.params[1]);
-        }
-    }
+        self.subst_q.set_freqs(pi);
+        self.freqs
+            .view_mut((0, 0), self.subst_q.freqs().shape())
+            .copy_from(self.subst_q.freqs());
+        pip_q(&mut self.q, self.subst_q.q(), self.params[1]);    }
 
     fn params(&self) -> &[f64] {
         &self.params
